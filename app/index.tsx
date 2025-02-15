0140abb3a75437beb11eb69d775dccb1e0220924
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2025-02-15 17:45:17 +0900

    Add Google signin

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,51 @@
 import { useRouter } from "expo-router";
 import { View } from "react-native";
 import { Button, useTheme } from "react-native-paper";
+import {
+  GoogleSignin,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import { supabase } from "@/utils/supabase";
 
 export default function Landing() {
   const theme = useTheme();
   const router = useRouter();
 
-  const goToMain = () => router.push('/statements')
+  GoogleSignin.configure({
+    scopes: [],
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLEINT_ID,
+  });
+
+  async function onPressGoogleSignin() {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      if (userInfo.data?.idToken) {
+        const { data, error } = await supabase.auth.signInWithIdToken({
+          provider: "google",
+          token: userInfo.data.idToken,
+        });
+        console.log(error, data);
+        if (error) {
+          throw error;
+        }
+        router.push("/statements");
+      } else {
+        throw new Error("no ID token present!");
+      }
+    } catch (error: any) {
+      console.error(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  }
 
   return (
     <View
@@ -19,7 +58,7 @@ export default function Landing() {
         alignItems: "center",
       }}
     >
-      <Button onPress={goToMain}>Go to main</Button>
+      <Button onPress={onPressGoogleSignin}>Signin with Google</Button>
     </View>
   );
 }
